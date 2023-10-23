1cff6d9ea075c483aa6f05c7f3d63e479a4a99a2
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-23 00:27:30 +0000

    Corrección de variables
    Se realizo un ligero cambio en la declaración de variables, se coloco el alcance que van  a tener y se cambio el nombre de max, por algo más descriptivo.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function valorMaximo(){
-  numeros = [1,2,3,4,5,6,7,8,20,10];
-  max = 0;
+   let numeros = [1,2,3,4,5,6,7,8,20,10];
+   let numeroMaximo = 0;
 
   for(i = 0; i < numeros.length; i++){
-    if(numeros[i] > max){
-      max = numeros[i];
+    if(numeros[i] > numeroMaximo){
+      numeroMaximo = numeros[i];
     }
   }
-  console.log(max);
+  console.log(numeroMaximo);
 }
 valorMaximo();
