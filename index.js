fe257a2dca5c5cccc472ffe177fd86c8b50966e7
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-23 00:37:38 +0000

    Reestructuración
    Se reestructuro el código en base a lo visto en la clase #7.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-function valorMaximo(){
-   let numeros = [1,2,3,4,5,6,7,8,20,10];
-   let numeroMaximo = 0;
+  let numeros = [1,2,3,4,5,6,7,8,20,10];
+  let numeroMaximo = 0;
+  let tamano = numeros.length;
 
-  for(i = 0; i < numeros.length; i++){
-    if(numeros[i] > numeroMaximo){
+  for(i = 0; i<tamano; i++){
+    if(numeroMaximo < numeros[i]){
       numeroMaximo = numeros[i];
     }
   }
   console.log(numeroMaximo);
-}
-valorMaximo();
