0d83671958f56f89fc6098b6687f34aa3d9c1227
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-25 22:11:24 +0000

    Creación de un nuevo archivo
    Se creo un nuevo archivo para hacer un substring de una frase, y se renombro un archivo para encontrar el número mayor

--- numeromaximo.js
+++ numeromaximo.js
@@ -1,13 +1,13 @@
-  let numeros = [1,2,3,4,5,6,7,8,20,10];
-  let numeroMaximo = 0;
-  let tamano = numeros.length;
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 20, 10];
+let numeroMaximo = 0;
+let tamano = numeros.length;
 
-  for(i = 0; i<tamano; i++){
-    if(numeroMaximo < numeros[i]){
-      numeroMaximo = numeros[i];
-    }
+for (i = 0; i < tamano; i++) {
+  if (numeroMaximo < numeros[i]) {
+    numeroMaximo = numeros[i];
   }
-  console.log(numeroMaximo);
+}
+console.log(numeroMaximo);
 
-  let maxima = Math.max(1,2,3,4,5,6,7,8,20,10);
+let maxima = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 20, 10);
 console.log(maxima);
