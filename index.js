04c61310bd0635c1a8e645549144431f4d6559b4
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-25 22:09:10 +0000

    Creación de un nuevo archivo
    Se renombro un archivo, se creo otro archivo para hacer un substring de una oración

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-  let numeros = [1,2,3,4,5,6,7,8,20,10];
-  let numeroMaximo = 0;
-  let tamano = numeros.length;
-
-  for(i = 0; i<tamano; i++){
-    if(numeroMaximo < numeros[i]){
-      numeroMaximo = numeros[i];
-    }
-  }
-  console.log(numeroMaximo);
-
-  let maxima = Math.max(1,2,3,4,5,6,7,8,20,10);
-console.log(maxima);
+let Frase = "Nunca pares de aprender";
+let inicial = 6;
+let final = 11;
+let palabra = Frase.substring(inicial,final);
+console.log(palabra);
