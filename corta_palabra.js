fcba94736e753de78fe496d18f288af72e356cf3
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-29 20:32:56 +0000

    Desafío 2:Operación matemática oculta.
    El problema nos nuestra las siguiente operaciones:
    5+4 = 19
    8+2 = 610
    10+8 =218
    12+9 = 321
    18+2 = 1620
    21+5 = 1626
    Debíamos de encontrar el por que del resultado, y analizando el problema es por que está el resultado de la suma y la resta de los mismos números concatenados

---
+++ corta_palabra.js
@@ -0,0 +1,5 @@
+let Frase = "Nunca pares de aprender";
+let inicial = 6;
+let final = 11;
+let palabra = Frase.substring(inicial, final);
+console.log(palabra);
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let Frase = "Nunca pares de aprender";
-let inicial = 6;
-let final = 11;
-let palabra = Frase.substring(inicial, final);
-console.log(palabra);
+function concatenacion(valor1, valor2){
+  let resta = valor1 - valor2;
+  let suma = valor1 + valor2;
+  let resultado = String(resta) + String(suma);
+  
+  console.log(resultado);
+}
+
+concatenacion(5,4);
+concatenacion(8,2);
+concatenacion(10,8);
+concatenacion(12,9);
+concatenacion(18,2);
+concatenacion(21,5);
